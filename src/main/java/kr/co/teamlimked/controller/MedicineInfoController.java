0f6a1d2fa8e4bcab5bde0915d88f2e804819e8e3
RosieOh <20172207@gm.hannam.ac.kr>
2023-12-19 00:31:01 +0900

    🌈Design : 게시판 디자인으로 사용할 ClEditor 추가🌈

--- src/main/java/kr/co/teamlimked/controller/MedicineInfoController.java
+++ src/main/java/kr/co/teamlimked/controller/MedicineInfoController.java
@@ -7,7 +7,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@Log4j2
 @Controller
 @RequiredArgsConstructor
 @RequestMapping("/medicine/*")
